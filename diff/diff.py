b07c1ab24615bbe5852df404b5f4a646127c1468
Vanya Io <differentstuff777@gmail.com>
2019-05-10 08:45:02 +0300

    working choosing step

--- diff/diff.py
+++ diff/diff.py
@@ -53,7 +53,7 @@ def rk(f, y0, a, b, h, s=2):
 	y_step = y0
 	
 	it = 1
-	while x_step < (b - h):
+	while x_step < b:
 		y.append(rk_step(f, x_step, h, y_step, s))
 		y_step = np.array(y[len(y)-1])
 		x_step += h
@@ -74,7 +74,7 @@ def true_solution(a, b, h):
 	#return np.array([y1(x), y2(x), y3(x), y4(x)])
 	y = []
 	x = a
-	while x < b:
+	while x <= b:
 		y.append(np.array([y1(x), y2(x), y3(x), y4(x)]))
 		x += h
 	
@@ -87,16 +87,16 @@ def runge_local_error(ys, yss, p=2):
 def get_h_tol(h, rn, p=2):
 	return h * ((tol / rn) ** (1.0 / p))
 
-def rk_with_step(f, y0, a, b, h0=, s=2,p=2):
-	tol = 
-	y = [y0]
+def rk_with_step(f, y0, a, b, h0=1.0, s=2,p=2):
+	tol = 1e-10
+	y = [(y0, a)]
 	x_step = a
 	y_step = y0
 
 	h = h0
-	while x_step < (b - h):
-		y1 = rk_step(f, x_step, h, y_step, s)
-		yss = rk_step(f, x_step + h, h, y1, s)
+	while x_step < b:
+		y1 = rk_step(f, x_step, h * 0.5, y_step, s)
+		yss = rk_step(f, x_step + h * 0.5, h * 0.5, y1, s)
 		ys = rk_step(f, x_step, h, y_step, s)
 		rn = runge_local_error(ys, yss, p)
 		
@@ -107,16 +107,18 @@ def rk_with_step(f, y0, a, b, h0=, s=2,p=2):
 		if (tol < rn) and (rn <= (tol * (2 ** p))):
 			h *= 0.5
 			y_step = yss
-			continue
 
 		if (tol * (2 ** (-p-1)) <= rn) and (rn <= tol):
 			y_step = ys
-			continue
 		
 		if rn < tol * (2 ** (-p-1)):			
 			h = 2 * h
 			y_step = ys
-
+		
+		x_step += h
+		y.append((y_step, x_step))
+	
+	return y	
 
 def main1():
 	A = 1.0
@@ -136,7 +138,36 @@ def main1():
 	res2 = rk(fun, y0, a, b, h, s=4)
 	print("4s solution:")
 	print(res2)	 
-	
+
+	#def rk_with_step(f, y0, a, b, h0=1.0, s=2,p=2):
+	res3 = rk_with_step(fun, y0, a, b)
+	#print("2s solution with auto step:")
+	#print(res3) 
+	A = 1.0
+	B = 1.5
+	C = -2.0
+	y1 = lambda x: exp(sin(x**2))
+	y2 = lambda x: exp(B * sin(x**2))
+	y3 = lambda x: C * sin(x**2) + A
+	y4 = lambda x: cos(x**2)
+	'''
+	for i in range(len(res3) - 1):
+		y = res3[i][0]
+		x = res3[i][1]	
+		print("*"*20)
+		true_y = np.array([y1(x), y2(x), y3(x), y4(x)])
+		print(y)
+		print(true_y)
+		print(y - true_y)	
+	'''
+	i = len(res3)-1
+	y = res3[i][0]
+	x = res3[i][1]
+	print(len(res3))
+	print(x)
+	print(y)
+	print(np.array([y1(x), y2(x), y3(x), y4(x)]))
+
 	print("diff (true&2s):")
 	for i in range(len(res_true) - 1):
 		print(la.norm(res1[i] - res_true[i]))
