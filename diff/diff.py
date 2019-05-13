77328b6f1c865b0c0331330723e0d49a02be8df6
Vanya Io <differentstuff777@gmail.com>
2019-05-13 16:47:57 +0300

    written but not checked

--- diff/diff.py
+++ diff/diff.py
@@ -5,9 +5,12 @@ import scipy.special as special
 import random
 from functools import reduce
 import os
-from math import sin, cos, sinh, exp, cosh
+from math import sin, cos, sinh, exp, cosh, log
 import math
 import time
+import matplotlib.pyplot as plt
+import sys
+import os
 
 def rk_step(f, x0, h, y0, s=2): 
 	c2 = 0.25
@@ -34,6 +37,7 @@ def rk_step(f, x0, h, y0, s=2):
 		K[3] = f(x0 + h, y0 + h * K[2])
 		return y0 + h * ( (1.0/6)*K[0] + (1.0/3) * K[1] +(1.0/3) * K[2] + (1.0/6) * K[3])		
 
+Counter = 0
 def fun(x, y):
 	A = 1.0
 	B = 1.5
@@ -44,7 +48,9 @@ def fun(x, y):
 	res.append(2 * B * x * exp( (B / C) * (y[2] - A)) * y[3])
 	res.append(2 * C * x * y[3])
 	res.append(-2 * x * np.log(y[0]))
-		
+	
+	global Counter
+	Counter += 1	
 	return np.array(res)
 
 def rk(f, y0, a, b, h, s=2):
@@ -81,14 +87,14 @@ def true_solution(a, b, h):
 	return np.array(y)
 
 def runge_full_error(yn, y2n, p=2):
-	return la.norm(y2n - yn) / ((2 ** p) - 1)
+	return la.norm(y2n - yn) / (-(2 ** -p) + 1)
 def runge_local_error(ys, yss, p=2):
 	return la.norm(yss - ys) / (-(2 ** -p) + 1)
 def get_h_tol(h, rn, p=2, tol=1e-5):
 	return h * ((tol / rn) ** (1.0 / p))
 
 def rk_with_step(f, y0, a, b, h0=1.0, s=2,p=2):
-	tol = 1e-10
+	tol = 1e-8#-10
 	y = [(y0, a)]
 	x_step = a
 	y_step = y0
@@ -118,41 +124,101 @@ def rk_with_step(f, y0, a, b, h0=1.0, s=2,p=2):
 		x_step += h
 		y.append((y_step, x_step))
 	
-	return y	
+	return y
 
-def main1():
+def rk_with_step_atol(f, y0, a, b, h0=1.0, s=2, p=2, rtol=1e-8):
+	atol = 1e-12
+	y = [(y0, a)]
+	x_step = a
+	y_step = y0
+
+	h = h0
+	while x_step < b:
+		y1 = rk_step(f, x_step, h * 0.5, y_step, s)
+		yss = rk_step(f, x_step + h * 0.5, h * 0.5, y1, s)
+		ys = rk_step(f, x_step, h, y_step, s)
+		rn = runge_local_error(ys, yss, p)
+		
+		tol = rtol * la.norm(ys) + atol
+		if rn > (tol * (2 ** p)):	
+			h *= 0.5			
+			continue
+
+		if (tol < rn) and (rn <= (tol * (2 ** p))):
+			h *= 0.5
+			y_step = yss
+
+		if (tol * (2 ** (-p-1)) <= rn) and (rn <= tol):
+			y_step = ys
+		
+		if rn < tol * (2 ** (-p-1)):			
+			h = 2 * h
+			y_step = ys
+		
+		x_step += h
+		y.append((y_step, x_step))
+	
+	return y
+	
+def main(s=2):
 	A = 1.0
 	a = 0.0
 	b = 5.0
 	y0 = np.array([1.0, 1.0, 1.0, A])
-	#h = (b - a) / 8
-	h = 1.0 / (2 ** 10)
+
+	if s == 4:
+		k0 = 3
+	if s == 2:
+		k0 = 10
+	h = 1.0 / (2 ** k0)#10
+
 	print("true solution:")
 	res_true = true_solution(a, b, h)
 	print(res_true)
+	#1:
+	'''
+	x1 = []
+	y1 = []
+	x2 = []
+	y2 = []
+	for k in range(6):
+		h1 = 1.0 / (2 ** (k0 + k))
+		res = rk(fun, y0, a, b, h1, s=s)
+		r = la.norm(res_true[-1] - res[-1])
+		x1.append(k + k0)
+		y1.append(log(r, 2))
+		x2.append(k + k0)
+		y2.append(-s * (k + k0))
+	x1 = np.array(x1)
+	y1 = np.array(y1)
+	x2 = np.array(x2)
+	y2 = np.array(y2)
+	plt.plot(x1, y1, label='error')
+	plt.plot(x2, y2, label='linear f')
+	plt.legend()
+	plt.show()
+	'''
 
-	res1 = rk(fun, y0, a, b, h)
-	print("2s solution:")
+	#2:
+	res1 = rk(fun, y0, a, b, h, s=s)
+	print(str(s) + "s solution:")
 	print(res1)	 
-	res12 = rk(fun, y0, a, b, h * 0.5)
-	print("2s2 solution:")
+	res12 = rk(fun, y0, a, b, h * 0.5, s=s)
+	print(str(s) + "s h/2 solution:")
 	print(res12)	 
-	rn = runge_full_error(res1[-1], res12[-1])
-	h_tol = get_h_tol(rn, h)
+	rn = runge_full_error(res1[-1], res12[-1], p=s)
+	h_tol = get_h_tol(h, rn, p=s)
 	print("h tol: " + str(h_tol))
 	
-	res_h_tol = rk(fun, y0, a, b, h_tol)
-	print("h tol:")
+	res_h_tol = rk(fun, y0, a, b, h_tol, s=s)
+	print("h tol solution:")
 	print(res_h_tol)
-	
-	res2 = rk(fun, y0, a, b, h, s=4)
-	print("4s solution:")
-	print(res2)	 
 
-	#def rk_with_step(f, y0, a, b, h0=1.0, s=2,p=2):
-	res3 = rk_with_step(fun, y0, a, b)
-	#print("2s solution with auto step:")
-	#print(res3) 
+	print("h_tol and true solution compare:")
+	print(la.norm(res_h_tol[-1] - res_true[-1]))
+
+	xa = []
+	ya = []
 	A = 1.0
 	B = 1.5
 	C = -2.0
@@ -160,6 +226,19 @@ def main1():
 	y2 = lambda x: exp(B * sin(x**2))
 	y3 = lambda x: C * sin(x**2) + A
 	y4 = lambda x: cos(x**2)
+
+	x_step = a
+	for y_tol in res_h_tol:
+		xa.append(x_step)
+		ya.append(la.norm(np.array([y1(x_step), y2(x_step), y3(x_step), y4(x_step)]) - y_tol))
+		x_step += h_tol		
+	xa = np.array(xa)
+	ya = np.array(ya)
+	plt.plot(xa, ya)
+	plt.show()
+
+	#AUTO  STEP PART
+	res3 = rk_with_step_atol(fun, y0, a, b, s=s, p=s)
 	'''
 	for i in range(len(res3) - 1):
 		y = res3[i][0]
@@ -170,23 +249,75 @@ def main1():
 		print(true_y)
 		print(y - true_y)	
 	'''
-	i = len(res3)-1
-	y = res3[i][0]
-	x = res3[i][1]
+	y = res3[-1][0]
 	print(len(res3))
-	print(x)
+	print("solutuion with auto step:")
 	print(y)
-	print(np.array([y1(x), y2(x), y3(x), y4(x)]))
+	#print("true solut:")
+	#print(np.array([y1(x), y2(x), y3(x), y4(x)]))
+	
+	#1
+	'''
+	xa = []
+	ya = []
+	for sol in res3:
+		xa.append(sol[1])
+		ya.append(sol[0])
+	xa = np.array(xa)
+	ya = np.array(ya)
+	plt.plot
+	'''
+	#2
+	xa = []
+	ya = []
+	for i in range(len(res3) - 1):
+		xnow = res3[i][1]
+		xnext = res3[i+1][1]
+		xa.append(xnow)
+		ya.append(xnext - xnow)
+	xa = np.array(xa)
+	ya = np.array(ya)
+	plt.plot(xa, ya)
+	plt.show()
 
-	print("diff (true&2s):")
-	for i in range(len(res_true) - 1):
-		print(la.norm(res1[i] - res_true[i]))
+	#3
+	xa = []
+	ya = []
+	for sol in res3:
+		x_step = sol[1]
+		xa.append(x_step)
+		ya.append(la.norm(np.array([y1(x_step), y2(x_step), y3(x_step), y4(x_step)]) - sol[0]))
+	
+	xa = np.array(xa)
+	ya = np.array(ya)
+	plt.plot(xa, ya)
+	plt.show()
 
+	#4:
+	xa = []
+	ya = []
+	global Counter
+	rtol_list = [1e-6, 1e-7, 1e-8]
+	for rtol in rtol_list:
+		Counter = 0
+		res = rk_with_step_atol(fun, y0, a, b, s=s, p=s, rtol=rtol)
+		xa.append(log(rtol, 2))		
+		ya.append(log(Counter, 2))
+
+	plt.plot(np.array(xa), np.array(ya))
+	plt.show()
 
 if __name__ == "__main__":
-	main1()
-
-
+	if len(sys.argv) == 1:
+		main(s=2)
+		print("*"*20)
+		main(s=4) 	
+		os._exit(0)
+	
+	if sys.argv[1] == '2':
+		main(s=2)
+	if sys.argv[1] == '4':
+		main(s=4)
 
 
 
